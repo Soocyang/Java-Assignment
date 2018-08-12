035053d55b86afcb04cfbfb069dc670a9cee1f33
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2018-08-12 16:34:49 +0800

    missed upload name.java

---
+++ src/Name.java
@@ -0,0 +1,21 @@
+public class Name {
+    private String firstName;
+    private String lastName;
+
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
+
+
+
